d852fee08baff04305d5c4c6a1a83a9d7e7a7d2b
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-11-13 15:43:46 +0100

    [~] renamed everything appropriately

--- ClackosProj2/Form1.Designer.cs
+++ ClackosProj2/Form1.Designer.cs
@@ -28,44 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.header = new System.Windows.Forms.Panel();
             this.searchBtn = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.filterPanel = new System.Windows.Forms.Panel();
+            this.header.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // title
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(324, 39);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "PrYson V2 APP EDITION";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(3, 3);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(324, 39);
+            this.title.TabIndex = 0;
+            this.title.Text = "PrYson V2 APP EDITION";
+            this.title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // panel1
+            // header
             // 
-            this.panel1.AutoSize = true;
-            this.panel1.Controls.Add(this.searchBtn);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 48);
-            this.panel1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(502, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(201, 26);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.header.AutoSize = true;
+            this.header.Controls.Add(this.searchBtn);
+            this.header.Controls.Add(this.searchBox);
+            this.header.Controls.Add(this.title);
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(0, 0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(800, 48);
+            this.header.TabIndex = 1;
             // 
             // searchBtn
             // 
@@ -77,16 +70,32 @@
             this.searchBtn.UseVisualStyleBackColor = true;
             this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(502, 12);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(201, 26);
+            this.searchBox.TabIndex = 1;
+            this.searchBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // filterPanel
+            // 
+            this.filterPanel.Location = new System.Drawing.Point(10, 54);
+            this.filterPanel.Name = "filterPanel";
+            this.filterPanel.Size = new System.Drawing.Size(250, 384);
+            this.filterPanel.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.filterPanel);
+            this.Controls.Add(this.header);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.header.ResumeLayout(false);
+            this.header.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,10 +103,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel header;
         private System.Windows.Forms.Button searchBtn;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Panel filterPanel;
     }
 }
 
