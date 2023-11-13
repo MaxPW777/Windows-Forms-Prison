1beb793fa2f7b518de41b7121cf736c74cb947c8
ludogernay <ludovic.gernay@ynov.com>
2023-11-13 15:35:35 +0100

    [+] display database info...

--- ClackosProj2/Form1.Designer.cs
+++ ClackosProj2/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace ClackosProj2
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+
+namespace ClackosProj2
 {
     partial class Form1
     {
@@ -6,6 +10,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        
 
         /// <summary>
         /// Clean up any resources being used.
@@ -19,7 +24,7 @@
             }
             base.Dispose(disposing);
         }
-
+       
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -30,8 +35,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.searchBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,9 +46,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Location = new System.Drawing.Point(3, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(324, 39);
+            this.label1.Size = new System.Drawing.Size(271, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "PrYson V2 APP EDITION";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -55,36 +61,50 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 48);
+            this.panel1.Size = new System.Drawing.Size(711, 38);
             this.panel1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(502, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(201, 26);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // searchBtn
             // 
-            this.searchBtn.Location = new System.Drawing.Point(709, 12);
+            this.searchBtn.Location = new System.Drawing.Point(630, 10);
+            this.searchBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.searchBtn.Name = "searchBtn";
-            this.searchBtn.Size = new System.Drawing.Size(79, 29);
+            this.searchBtn.Size = new System.Drawing.Size(70, 23);
             this.searchBtn.TabIndex = 2;
             this.searchBtn.Text = "Search";
             this.searchBtn.UseVisualStyleBackColor = true;
             this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(446, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(51, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(600, 200);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -98,6 +118,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button searchBtn;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ClackosProj2/Form1.cs
+++ ClackosProj2/Form1.cs
@@ -1,22 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ClackosProj2
 {
     public partial class Form1 : Form
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
         public Form1()
         {
             InitializeComponent();
+            LoadDataFromDatabase();
         }
+        private void LoadDataFromDatabase()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
 
+                    // Exécutez vos opérations sur la base de données ici
+                    // Par exemple, exécutez une requête SQL pour récupérer des données
+
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM VotreTable", connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Lisez les données du lecteur et utilisez-les comme nécessaire
+                            string data = reader.GetString(0);
+                            label2.Text = data;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Gérez les erreurs ici
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -25,6 +58,11 @@ namespace ClackosProj2
         private void searchBtn_Click(object sender, EventArgs e)
         {
             Console.WriteLine();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
